06cddf2cb2856d2380714117241e1d56fbc1de06
syrnil <simonequarta08@gmail.com>
2025-09-30 22:24:04 +0200

    Create esercizio1.js

---
+++ esercizio1.js
@@ -0,0 +1,16 @@
+
+
+
+const { createApp } = Vue;
+
+
+createApp({
+    data() {
+        return {
+            ESERCIZIO:1,
+            nome:"simone",
+            cognome:"quarta",
+            citta:"moncalieri",
+            prodotto:"cereali",
+            prezzo:"5.50€",
+        }
